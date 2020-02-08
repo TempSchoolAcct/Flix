7b1500f13ed0624e756020c5cb4cd15d0c4884ce
Jane <JaneBethPerez@gmail.com>
2020-02-07 18:55:12 -0600

    Created the MovieDetailViewControl and connected the movie detail view controller to the table view cells

---
+++ Flix/MoviesDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesDetailsViewController.swift
+//  Flix
+//
+//  Created by Socstudent on 2/7/20.
+//  Copyright © 2020 Socstudent. All rights reserved.
+//
+
+import UIKit
+
+class MoviesDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
