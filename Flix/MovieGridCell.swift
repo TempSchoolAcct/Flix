71b79cc48da325b2f6672d84e4f59147e3e1f1cc
Jane <JaneBethPerez@gmail.com>
2020-02-09 18:03:48 -0600

    Created the tab bar controller, created the superhero collection view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Socstudent on 2/9/20.
+//  Copyright © 2020 Socstudent. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
