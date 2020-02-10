ceb1f0c3258a3e91c456fa98842b112311ea75dc
Jane <JaneBethPerez@gmail.com>
2020-02-09 18:59:49 -0600

    The grid layout now correctly adjusts the size of the posters

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -27,7 +27,8 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         layout.minimumInteritemSpacing = 4
         
         let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
-        layout.itemSize =  CGSize(width: width, height: width * 3 / 2)
+        layout.itemSize = CGSize(width: width, height: width * 1.5)
+        
         
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
